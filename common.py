6d5ef553d791138484512de5f3f8278e291281cb
Tam Pham <thi.pham.2018@mumail.ie>
2019-02-13 23:23:39 +0000

    Define a common class for re-using purpose

--- common.py
+++ common.py
@@ -5,6 +5,7 @@ import datetime as dt
 class Common:
     CLEAN_DATA_DIR = "./saved-data"
     CLEAN_DATA_FILE_FULL_PATH = "./saved-data/db_all_data.csv"
+    #CLEAN_DATA_FILE_FULL_PATH = "./saved-data/db_new_data.csv"
     PLOTS_DIR = "./plots"
     CLUSTERING_PLOTS_DIR = "./plots/clustering"
     SHORT_WEEKDAY_ORDER = ['Mon','Tue','Wed','Thu','Fri','Sat', 'Sun']
@@ -16,6 +17,15 @@ class Common:
                 os.makedirs(directory)
         except OSError:
             print("Error while creating folder " + directory)
+
+    @staticmethod
+    def getWorkingDirectory():
+        return os.getcwd()
+
+    @staticmethod
+    def goToSubDirectory(subDirectory):
+        new_dir = os.path.join(Common.getWorkingDirectory(), subDirectory)
+        os.chdir(new_dir)
 
     @staticmethod
     def saveCSV(df, filePath):
